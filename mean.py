820684e691b0e9e1c7667ddc19380dcdf8ea64c4
palladium-o <149358965+palladium-o@users.noreply.github.com>
2023-11-04 15:09:21 +0000

    Update mean.py

--- mean.py
+++ mean.py
@@ -1,11 +1,8 @@
 nums = [3, 1, 2, 5, 1, 5, -7, 9, -8, -3, 3]
 
-l = len(nums) - 1
-
 diff_list = []
 
-for i in range(l):
-    diff = nums[0] - nums[1]
+for i in enumerate(nums) - 1
     if diff < 0:
         diff = nums[1] - nums[0]
     diff_list.append(diff)
