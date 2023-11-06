69aa810743ec423195cf9d8303f692c3f522d6a7
Yerma Ismyne <149358965+palladium-o@users.noreply.github.com>
2023-11-06 01:36:30 +0000

    Create mean_diff_lcomp.py

---
+++ mean_diff_lcomp.py
@@ -0,0 +1,10 @@
+nums = [3, 1, 2, 5, 1, 5, -7, 9, -8, -3, 3]
+
+diff_list = [
+   abs(a - b) 
+   for a, b in zip(nums, nums[1:])
+]
+
+mean = sum(diff_list) / len(diff_list)
+
+print(f'Mean: {mean:.1f}')
