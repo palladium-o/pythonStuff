ff7462e665bef93208ee04f82477395eb5bccce6
Yerma Ismyne <149358965+palladium-o@users.noreply.github.com>
2023-11-06 00:45:37 +0000

    Update mean_calculator.py

--- mean_calculator.py
+++ mean_calculator.py
@@ -2,7 +2,8 @@ nums = [3, 1, 2, 5, 1, 5, -7, 9, -8, -3, 3]
 
 diff_list = []
 
-for i in enumerate(nums) - 1:
+for i in range(len(nums)):
+    diff = nums[0] - nums[1]
     if diff < 0:
         diff = nums[1] - nums[0]
     diff_list.append(diff)
@@ -10,4 +11,4 @@ for i in enumerate(nums) - 1:
 
 mean = sum(diff_list) / len(diff_list)
 
-print(round(mean, 1))
+print(f'mean:.1f)
